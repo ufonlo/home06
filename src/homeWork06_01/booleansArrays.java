8b57ef72e5ff8da6577fa1e0d0002ba6986dc588
ufonlo <socufo@gmail.com>
2018-05-14 12:42:33 +0300

    one

---
+++ src/homeWork06_01/booleansArrays.java
@@ -0,0 +1,12 @@
+package homeWork06_01;
+
+public class booleansArrays {
+	public static void main(String[] args) {
+		int Size = 30;
+		boolean ArraysMasive[] = new boolean[Size];
+
+		for (int i = 0; i < ArraysMasive.length; i++) {
+			System.out.println(ArraysMasive[i]);
+		}
+	}
+}
